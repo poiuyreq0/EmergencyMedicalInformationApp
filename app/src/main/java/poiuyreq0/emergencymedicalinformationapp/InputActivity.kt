e5940c92383b07cdb2e99139162a0da6936625c1
poiuyreq0 <poiuyreq0@gmail.com>
2023-02-23 12:27:04 +0900

    project init

--- app/src/main/java/poiuyreq0/emergencymedicalinformationapp/InputActivity.kt
+++ app/src/main/java/poiuyreq0/emergencymedicalinformationapp/InputActivity.kt
@@ -2,10 +2,12 @@ package poiuyreq0.emergencymedicalinformationapp
 
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.core.view.isVisible
 import poiuyreq0.emergencymedicalinformationapp.databinding.ActivityInputBinding
 
@@ -42,5 +44,32 @@ class InputActivity : AppCompatActivity() {
         }
 
         binding.warningEditTextView.isVisible = binding.warningCheckBox.isChecked
+
+        binding.saveButton.setOnClickListener {
+            saveData()
+            finish()
+        }
+    }
+
+    private fun saveData() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE).edit()) {
+            putString(NAME, binding.nameEditTextView.text.toString())
+            putString(BIRTHDATE, binding.birthdateValueTextView.text.toString())
+            putString(BLOOD_TYPE, getBloodType())
+            putString(EMERGENCY_CONTACT, binding.emergencyContactEditTextView.text.toString())
+            putString(WARNING, getWarning())
+            apply()
+        }
+        Toast.makeText(this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getBloodType(): String {
+        val bloodAlphabet = binding.bloodTypeSpinner.selectedItem.toString()
+        val bloodSign = if (binding.bloodTypePlusRadioButton.isChecked) "+" else "-"
+        return "$bloodSign$bloodAlphabet"
+    }
+
+    private fun getWarning(): String {
+        return if (binding.warningCheckBox.isChecked) binding.warningEditTextView.text.toString() else ""
     }
 }
--- app/src/main/java/poiuyreq0/emergencymedicalinformationapp/MainActivity.kt
+++ app/src/main/java/poiuyreq0/emergencymedicalinformationapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package poiuyreq0.emergencymedicalinformationapp
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
 import poiuyreq0.emergencymedicalinformationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +19,48 @@ class MainActivity : AppCompatActivity() {
 
         binding.goInputActivityButton.setOnClickListener {
             val intent = Intent(this, InputActivity::class.java)
-
             startActivity(intent)
         }
+
+        binding.deleteButton.setOnClickListener {
+            deleteData()
+        }
+
+        binding.emergencyContactLayer.setOnClickListener {
+            with(Intent(Intent.ACTION_VIEW)) {
+                val phoneNumber = binding.emergencyContactValueTextView.text.toString().replace("-", "")
+                data = Uri.parse("tel:$phoneNumber")
+                startActivity(this)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        uiUpdate()
+    }
+
+    private fun uiUpdate() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)) {
+            binding.nameValueTextView.text = getString(NAME, "...")
+            binding.birthdateValueTextView.text = getString(BIRTHDATE, "...")
+            binding.bloodTypeValueTextView.text = getString(BLOOD_TYPE, "...")
+            binding.emergencyContactValueTextView.text = getString(EMERGENCY_CONTACT, "...")
+            var warning = getString(WARNING, "...")
+            binding.warningTextView.isVisible = warning.isNullOrEmpty().not()
+            binding.warningValueTextView.isVisible = warning.isNullOrEmpty().not()
+            if (!warning.isNullOrEmpty()) {
+                binding.warningValueTextView.text = warning
+            }
+        }
+    }
+
+    private fun deleteData() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE).edit()) {
+            clear()
+            apply()
+        }
+        uiUpdate()
+        Toast.makeText(this, "모든 데이터를 삭제했습니다.", Toast.LENGTH_SHORT).show()
     }
 }
